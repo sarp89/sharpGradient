a70946f93639ffc6855a95cd7a2820c8bd29cd28
sarp89 <43776066+sarp89@users.noreply.github.com>
2019-01-23 09:51:20 +0100

    Update bar.js

--- bar.js
+++ bar.js
@@ -16,29 +16,3 @@ Chart.pluginService.register({
     }
   }
 });
-    
-new Chart(document.getElementById("bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      chartArea: {
-          backgroundColorOne: 'rgba(255,113,113,1)',
-          backgroundColorTwo: 'rgba(255,251,123,1)',
-          backgroundColorThree: 'rgba(85,203,140,1)',
-      },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
-});
